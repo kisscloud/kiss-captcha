99e8a5637147c780d37bd8a235830cf354aab399
qianrongli <qianrongli@uchain.one>
2018-11-09 18:32:51 +0800

    create

---
+++ common/src/main/java/com/kiss/captcha/KissCaptchaCommonApplication.java
@@ -0,0 +1,13 @@
+package com.kiss.captcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KissCaptchaCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KissCaptchaCommonApplication.class);
+    }
+
+}
